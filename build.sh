f72496fbd6e380657e32e5903590be0bc8d1f7f7
Aaron Lidman <aaronlidman@gmail.com>
2013-10-26 00:46:12 -0700

    Initial run
    Don't remember substitution notation

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+SOURCES=$(find */ -type f -name '*.json' | sort)
 
 for f in $SOURCES
 do
-    echo "\t\"$f\": {contents}" >> name-suggestions.json
+    echo "    \""${f%".json"}"\":" >> name-suggestions.json
+    cat $f >> name-suggestions.json
+    echo "," >> name-suggestions.json
 done
-
