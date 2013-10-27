491305a4c05c526316afd4088cdbb4775e99a8d0
Aaron Lidman <aaronlidman@gmail.com>
2013-10-27 13:06:52 -0700

    Basic read/write

--- build.js
+++ build.js
@@ -1,15 +1,37 @@
-var find = require('findit'),
+var fs = require('fs'),
+    find = require('findit'),
+    jsonlint = require('jsonlint');
+
+var outfile = 'name-suggestion-index.json',
     keys = ['amenity', 'shop'],
-    finder = find('.');
+    finder = find('.'),
+    files = [];
+
+fs.unlinkSync(outfile);
 
 finder.on('file', function (file) {
-    // on each json file
     if (file.split('.json').length > 1) {
-        var loc = file.split('/');
-        // if that file is in a dir that matches an item in the keys array
-        if (loc.length > 1 && keys.indexOf(loc[0]) != -1) {
-            // do stuff with it
-            console.log(file);
+        if (keys.indexOf(file.split('/')[0]) != -1) {
+            files.push(file);
         }
+    }
+});
+
+finder.on('end', function() {
+    var version = '// ' + Math.round(new Date().getTime()/1000) + '\n';
+    fs.appendFileSync(outfile, version);
+    // first line comment with unixtime and version
+        // can we get version from package.json? it is json right?
+        // var version = fs.readFileSync('package.json', 'utf8');
+        // version = jsonlint.parse(version).version;
+
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i],
+            tagCombo = file.split('.json')[0],
+            data = {};
+    
+        file = fs.readFileSync(file, 'utf8');
+        data[tagCombo] = jsonlint.parse(file);
+        fs.appendFileSync(outfile, JSON.stringify(data, null, 4));
     }
 });
