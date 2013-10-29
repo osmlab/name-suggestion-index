8a3eef5feccaea766f3586682ccf4c43f84d609b
Aaron Lidman <aaronlidman@gmail.com>
2013-10-29 14:23:35 -0700

    getRaw

---
+++ getRaw.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+var osmium = require('/Users/aaron/libosmium/node-osmium/lib/osmium.js');
+
+var start = new Date().getTime();
+console.log('running');
+
+if (process.argv.length != 4) {
+    console.log("Usage: " + process.argv[0] + ' ' + process.argv[1] + " OSMFILE OUTFILE");
+    process.exit(1);
+}
+
+var input_filename = process.argv[2],
+    output_filename = process.argv[3],
+    handler = new osmium.Handler(),
+    osmKeys = ['amenity', 'shop'],
+    counts = {};
+
+handler.options({"tagged_nodes_only": true});
+
+handler.on('node', takeTags);
+handler.on('way', takeTags);
+handler.on('relation', takeTags);
+
+function takeTags(entity) {
+    if (entity.tags) {
+        for (var key in entity.tags) {
+            if (entity.tags.name && osmKeys.indexOf(key) != -1) {
+                var fullName = key + '/' + entity.tags[key] + '|' + entity.tags.name;
+                if (counts[fullName]) {
+                    counts[fullName] += 1;
+                } else {
+                    counts[fullName] = 1;
+                }
+            }
+        }
+    }
+}
+
+function done() {
+    var out = {};
+    for (var key in counts) {
+        if (counts[key] > 100) {
+            out[key] = counts[key];
+        }
+    }
+    console.log('> 100: ' + Object.keys(out).length);
+    console.log(out);
+}
+
+handler.on('done', function() {
+    done();
+    console.log('run time: ' + Math.round((new Date().getTime() - start)/1000));
+});
+
+var reader = new osmium.Reader(input_filename);
+reader.apply(handler, { "with_location_handler": false });
+// from what I understand
+    // with_location_handler determines if there is an effort made to compile the geometry
+    // true: ways go and find their node values and build wkt
+    // false: no effort is made with anything except nodes
+    // memory usage is much lower with false, it's a bit quicker too
+    // I'd love a way to specify exactly what I need for even more increased memory and performance
+        // handler.options({ "only": ['id', 'timestamp', 'tags'] })
