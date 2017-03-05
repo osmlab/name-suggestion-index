2e9824e0a2cab0456f4a206044225d79e8fa1b14
Max Erickson <maxerickson@gmail.com>
2017-03-05 14:04:27 -0500

    Add short explanation.

--- buildJosm.js
+++ buildJosm.js
@@ -7,12 +7,15 @@ var presets=xmlbuilder.create('presets', {version: '1.0', encoding: 'UTF-8'})
   .att('author', 'Name Suggestion Index');
 var topgroup=presets.ele('group').att('name','Name Sugg. Index');
 
+// for each name in name-suggestions.json, create a one-click
+// JOSM preset using the key and value structure from the json
+// to organize the presets into JOSM preset groups.
 for (key in suggest){
-    var group=topgroup.ele('group').att('name',key);
+    var keygroup=topgroup.ele('group').att('name',key);
     for (value in suggest[key]){
-	var subgroup=group.ele('group').att('name',value);
+	var valuegroup=keygroup.ele('group').att('name',value);
         for (name in suggest[key][value]){
-            var item=subgroup.ele('item')
+            var item=valuegroup.ele('item')
               .att('name', name);
               item.ele('key').att('key',key).att('value',value);
               item.ele('key').att('key','name').att('value',name);
