559936e9297dcdeabc633f841878a391aef329d0
Bryan Housel <bryan@mapbox.com>
2019-03-15 09:53:54 -0400

    can't use markdown (duh)

--- build_docs.js
+++ build_docs.js
@@ -163,7 +163,7 @@ function generatePage(tree, dict, k, v) {
 <a class="nav" href="../index.html">↑ Back to top</a>
 <div class="instructions">Some things you can do here:
 <ul>
-<li>Is a brand name missing? [Open an issue](https://github.com/osmlab/name-suggestion-index/issues) or pull request to add it!</li>
+<li>Is a brand name missing? <a target="_blank" href="https://github.com/osmlab/name-suggestion-index/issues">Open an issue</a> or pull request to add it!</li>
 <li>Click the "View on Overpass Turbo" link to see where the name is used in OpenStreetMap.</li>
 <li>If a record is missing a <code>'brand:wikidata'</code> tag, you can do the research to add it to our project, or filter it out if it is not a brand.<br/>
 See <a target="_blank" href="https://github.com/osmlab/name-suggestion-index/blob/master/CONTRIBUTING.md">CONTRIBUTING.md</a> for more info.</li>
