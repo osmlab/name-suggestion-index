8a5867d1b4201f81aa68e3ebd9121992c76a6d0f
Bryan Housel <bryan@mapbox.com>
2020-09-15 14:48:04 -0400

    Rebuild the duplicate name check - this change makes `nomatch` unnecessary
    Now: warn about "new" (no wikidata) entries that may duplicate an "existing" (has wikidata) item.
    The criteria for this warning is:
    - One of the items has no wikidata
    - The items have nearly the same name
    - The items have the same locationSet (or the one without wikidata is worldwide)

--- scripts/build_brands.js
+++ scripts/build_brands.js
@@ -270,6 +270,10 @@ function mergeItems() {
 // Checks all the items for several kinds of issues
 //
 function checkItems() {
+  const t = 'brands';
+  console.log('');
+  console.log('⚙️   ' + colors.yellow(`Checking ${t}...`));
+
   const oddPunctuation = /[\s\=!"#%'*{},.\/:?\(\)\[\]@\\$\^*+<>~`’\u00a1\u00a7\u00b6\u00b7\u00bf\u037e\u0387\u055a-\u055f\u0589\u05c0\u05c3\u05c6\u05f3\u05f4\u0609\u060a\u060c\u060d\u061b\u061e\u061f\u066a-\u066d\u06d4\u0700-\u070d\u07f7-\u07f9\u0830-\u083e\u085e\u0964\u0965\u0970\u0af0\u0df4\u0e4f\u0e5a\u0e5b\u0f04-\u0f12\u0f14\u0f85\u0fd0-\u0fd4\u0fd9\u0fda\u104a-\u104f\u10fb\u1360-\u1368\u166d\u166e\u16eb-\u16ed\u1735\u1736\u17d4-\u17d6\u17d8-\u17da\u1800-\u1805\u1807-\u180a\u1944\u1945\u1a1e\u1a1f\u1aa0-\u1aa6\u1aa8-\u1aad\u1b5a-\u1b60\u1bfc-\u1bff\u1c3b-\u1c3f\u1c7e\u1c7f\u1cc0-\u1cc7\u1cd3\u2016\u2017\u2020-\u2027\u2030-\u2038\u203b-\u203e\u2041-\u2043\u2047-\u2051\u2053\u2055-\u205e\u2cf9-\u2cfc\u2cfe\u2cff\u2d70\u2e00\u2e01\u2e06-\u2e08\u2e0b\u2e0e-\u2e16\u2e18\u2e19\u2e1b\u2e1e\u2e1f\u2e2a-\u2e2e\u2e30-\u2e39\u3001-\u3003\u303d\u30fb\ua4fe\ua4ff\ua60d-\ua60f\ua673\ua67e\ua6f2-\ua6f7\ua874-\ua877\ua8ce\ua8cf\ua8f8-\ua8fa\ua92e\ua92f\ua95f\ua9c1-\ua9cd\ua9de\ua9df\uaa5c-\uaa5f\uaade\uaadf\uaaf0\uaaf1\uabeb\ufe10-\ufe16\ufe19\ufe30\ufe45\ufe46\ufe49-\ufe4c\ufe50-\ufe52\ufe54-\ufe57\ufe5f-\ufe61\ufe68\ufe6a\ufe6b\uff01-\uff03\uff05-\uff07\uff0a\uff0c\uff0e\uff0f\uff1a\uff1b\uff1f\uff20\uff3c\uff61\uff64\uff65]+/g;
 
   let warnMatched = matcher.getWarnings();
@@ -280,12 +284,13 @@ function checkItems() {
   let warnMissingWikipedia = [];
   let warnMissingTag = [];
   let warnFormatTag = [];
-  let seen = {};
+  let seenName = {};
 
   let total = 0;
 
   // for now, process `brands/*` only
   const paths = Object.keys(_cache.path).filter(tkv => tkv.split('/')[0] === 'brands');
+  const display = (val) => `${val.displayName} (${val.id})`;
 
   paths.forEach(tkv => {
     const items = _cache.path[tkv];
@@ -302,61 +307,35 @@ function checkItems() {
       const name = tags.name || tags.brand;
       total++;
 
-//TODO - rethink what we want to accomplish with duplicat checking
-      // if (!parts.d) {  // ignore ambiguous items for these
-      //   // Warn if some other item matches this item
-      //   const m = matcher.matchParts(parts);
-      //   if (m && m.kvnd !== kvnd) {
-      //     warnMatched.push([m.kvnd, kvnd]);
-      //   }
-
-      //   // Warn if the name appears to be a duplicate
-      //   const stem = stemmer(name);
-      //   const other = seen[stem];
-      //   if (other) {
-      //     // suppress warning?
-      //     let suppress = false;
-      //     if (brands[other].nomatch && brands[other].nomatch.indexOf(kvnd) !== -1) {
-      //       suppress = true;
-      //     } else if (item.nomatch && item.nomatch.indexOf(other) !== -1) {
-      //       suppress = true;
-      //     }
-      //     if (!suppress) {
-      //       warnDuplicate.push([kvnd, other]);
-      //     }
-      //   }
-      //   seen[stem] = kvnd;
-      // }
-
       // Warn if `brand:wikidata` or `brand:wikipedia` tags are missing or look wrong..
       const wd = tags['brand:wikidata'];
       if (!wd) {
-        warnMissingWikidata.push(item.id);
+        warnMissingWikidata.push(display);
       } else if (!/^Q\d+$/.test(wd)) {
-        warnFormatWikidata.push([item.id, wd]);
+        warnFormatWikidata.push([display(item), wd]);
       }
       const wp = tags['brand:wikipedia'];
       if (!wp) {
-        warnMissingWikipedia.push(item.id);
+        warnMissingWikipedia.push(display(item));
       } else if (!/^[a-z_]{2,}:[^_]*$/.test(wp)) {
-        warnFormatWikipedia.push([item.id, wp]);
+        warnFormatWikipedia.push([display(item), wp]);
       }
 
       // Warn on other missing tags
       switch (kv) {
         case 'amenity/gambling':
         case 'leisure/adult_gaming_centre':
-          if (!tags.gambling) { warnMissingTag.push([item.id, 'gambling']); }
+          if (!tags.gambling) { warnMissingTag.push([display(item), 'gambling']); }
           break;
         case 'amenity/fast_food':
         case 'amenity/restaurant':
-          if (!tags.cuisine) { warnMissingTag.push([item.id, 'cuisine']); }
+          if (!tags.cuisine) { warnMissingTag.push([display(item), 'cuisine']); }
           break;
         case 'amenity/vending_machine':
-          if (!tags.vending) { warnMissingTag.push([item.id, 'vending']); }
+          if (!tags.vending) { warnMissingTag.push([display(item), 'vending']); }
           break;
         case 'shop/beauty':
-          if (!tags.beauty) { warnMissingTag.push([item.id, 'beauty']); }
+          if (!tags.beauty) { warnMissingTag.push([display(item), 'beauty']); }
           break;
       }
 
@@ -364,29 +343,57 @@ function checkItems() {
       ['cuisine', 'vending', 'beauty', 'gambling'].forEach(osmkey => {
         const val = tags[osmkey];
         if (val && oddPunctuation.test(val)) {
-          warnFormatTag.push([item.id, `${osmkey} = ${val}`]);
+          warnFormatTag.push([display(item), `${osmkey} = ${val}`]);
         }
       });
       // Warn if user put `wikidata`/`wikipedia` instead of `brand:wikidata`/`brand:wikipedia`
       ['wikipedia', 'wikidata'].forEach(osmkey => {
         const val = tags[osmkey];
         if (val) {
-          warnFormatTag.push([item.id, `${osmkey} = ${val}`]);
+          warnFormatTag.push([display(item), `${osmkey} = ${val}`]);
         }
       });
-    });
 
+
+      // Warn about "new" (no wikidata) entries that may duplicate an "existing" (has wikidata) item.
+      // The criteria for this warning is:
+      // - One of the items has no wikidata
+      // - The items have nearly the same name
+      // - The items have the same locationSet (or the one without wikidata is worldwide)
+      const stem = stemmer(name) || name;
+      const itemwd = wd;
+      const itemls = loco.validateLocationSet(item.locationSet).id;
+
+      if (!seenName[stem]) seenName[stem] = new Set();
+      seenName[stem].add(item);
+
+      if (seenName[stem].size > 1) {
+        seenName[stem].forEach(other => {
+          if (other.id === item.id) return;   // skip self
+          const otherwd = other.tags['brand:wikidata'];
+          const otherls = loco.validateLocationSet(other.locationSet).id;
+
+          // pick one of the items without a wikidata tag to be the "duplicate"
+          if (!itemwd && (itemls === otherls || itemls === '+[Q2]')) {
+            warnDuplicate.push([display(item), display(other)]);
+          } else if (!otherwd && (otherls === itemls || otherls === '+[Q2]')) {
+            warnDuplicate.push([display(other), display(item)]);
+          }
+        });
+      }
+
+    });
   });
 
   if (warnMatched.length) {
-    console.warn(colors.yellow('\nWarning - items match other items:'));
+    console.warn(colors.yellow('\n⚠️   Warning - Ambiguous matches:'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
-    console.warn(colors.gray('If the items are the different, make sure they have different locationSets (e.g. "us", "ca"'));
-    console.warn(colors.gray('If the items are the same, remove extra `matchTags` or `matchNames`.  Remember:'));
-    console.warn(colors.gray('- Name matching ignores letter case, punctuation, spacing, and diacritical marks (é vs e). '));
-    console.warn(colors.gray('  No need to add `matchNames` for variations in these.'));
-    console.warn(colors.gray('- Tag matching automatically includes other similar tags in the same match group.'));
-    console.warn(colors.gray('  No need to add `matchTags` for similar tags.  see `config/match_groups.json`'));
+    console.warn(colors.gray('  If the items are the different, make sure they have different locationSets (e.g. "us", "ca"'));
+    console.warn(colors.gray('  If the items are the same, remove extra `matchTags` or `matchNames`.  Remember:'));
+    console.warn(colors.gray('  - Name matching ignores letter case, punctuation, spacing, and diacritical marks (é vs e). '));
+    console.warn(colors.gray('    No need to add `matchNames` for variations in these.'));
+    console.warn(colors.gray('  - Tag matching automatically includes other similar tags in the same match group.'));
+    console.warn(colors.gray('    No need to add `matchTags` for similar tags.  see `config/match_groups.json`'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
     warnMatched.forEach(w => console.warn(
       colors.yellow('  "' + w[0] + '"') + ' -> matches? -> ' + colors.yellow('"' + w[1] + '"')
@@ -395,9 +402,9 @@ function checkItems() {
   }
 
   if (warnMissingTag.length) {
-    console.warn(colors.yellow('\nWarning - Missing tags for items:'));
+    console.warn(colors.yellow('\n⚠️   Warning - Missing tag:'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
-    console.warn(colors.gray('To resolve these, add the missing tag.'));
+    console.warn(colors.gray('  To resolve these, add the missing tag.'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
     warnMissingTag.forEach(w => console.warn(
       colors.yellow('  "' + w[0] + '"') + ' -> missing tag? -> ' + colors.yellow('"' + w[1] + '"')
@@ -406,9 +413,9 @@ function checkItems() {
   }
 
   if (warnFormatTag.length) {
-    console.warn(colors.yellow('\nWarning - Unusual OpenStreetMap tag:'));
+    console.warn(colors.yellow('\n⚠️   Warning - Unusual OpenStreetMap tag:'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
-    console.warn(colors.gray('To resolve these, make sure the OpenStreetMap tag is correct.'));
+    console.warn(colors.gray('  To resolve these, make sure the OpenStreetMap tag is correct.'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
     warnFormatTag.forEach(w => console.warn(
       colors.yellow('  "' + w[0] + '"') + ' -> unusual tag? -> ' + colors.yellow('"' + w[1] + '"')
@@ -417,10 +424,14 @@ function checkItems() {
   }
 
   if (warnDuplicate.length) {
-    console.warn(colors.yellow('\nWarning - Potential duplicate item names:'));
+    console.warn(colors.yellow('\n⚠️   Warning - Potential duplicate:'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
-    console.warn(colors.gray('To resolve these, remove the worse entry and add `matchNames`/`matchTags` properties on the better entry.'));
-    console.warn(colors.gray('To suppress this warning for items that really are different, add a `nomatch` property on both items.'));
+    console.warn(colors.gray('  If the items are two different businesses,'));
+    console.warn(colors.gray('    make sure they both have accurate locationSets (e.g. "us"/"ca") and wikidata identifiers.'));
+    console.warn(colors.gray('  If the items are duplicates of the same business,'));
+    console.warn(colors.gray('    add `matchTags`/`matchNames` properties to the item that you want to keep, and delete the unwanted item.'));
+    console.warn(colors.gray('  If the duplicate item is a generic word,'));
+    console.warn(colors.gray('    add a filter to config/filters.json and delete the unwanted item.'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
     warnDuplicate.forEach(w => console.warn(
       colors.yellow('  "' + w[0] + '"') + ' -> duplicates? -> ' + colors.yellow('"' + w[1] + '"')
@@ -429,9 +440,9 @@ function checkItems() {
   }
 
   if (warnFormatWikidata.length) {
-    console.warn(colors.yellow('\nWarning - Item with incorrect `wikidata` format:'));
+    console.warn(colors.yellow('\n⚠️   Warning - Incorrect `wikidata` format:'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
-    console.warn(colors.gray('To resolve these, make sure "*:wikidata" tag looks like "Q191615".'));
+    console.warn(colors.gray('  To resolve these, make sure "*:wikidata" tag looks like "Q191615".'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
     warnFormatWikidata.forEach(w => console.warn(
       colors.yellow('  "' + w[0] + '"') + ' -> "*:wikidata": ' + '"' + w[1] + '"'
@@ -440,9 +451,9 @@ function checkItems() {
   }
 
   if (warnFormatWikipedia.length) {
-    console.warn(colors.yellow('\nWarning - Item with incorrect `wikipedia` format:'));
+    console.warn(colors.yellow('\n⚠️   Warning - Incorrect `wikipedia` format:'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
-    console.warn(colors.gray('To resolve these, make sure "*:wikipedia" tag looks like "en:Pizza Hut".'));
+    console.warn(colors.gray('  To resolve these, make sure "*:wikipedia" tag looks like "en:Pizza Hut".'));
     console.warn(colors.gray('--------------------------------------------------------------------------------'));
     warnFormatWikipedia.forEach(w => console.warn(
       colors.yellow('  "' + w[0] + '"') + ' -> "*:wikipedia": ' + '"' + w[1] + '"'
