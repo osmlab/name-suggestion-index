a7cd09df27b71b4f3c457945c2e14c7061648f68
Bryan Housel <bryan@mapbox.com>
2020-09-18 10:55:58 -0400

    Same as previous, but commit the brand files too

--- index.mjs
+++ index.mjs
@@ -1,4 +1,3 @@
 export { default as matcher } from './lib/matcher.js';
 export { default as simplify } from './lib/simplify.js';
 export { default as stemmer } from './lib/stemmer.js';
-export { default as toParts } from './lib/to_parts.js';
