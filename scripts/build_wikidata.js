86ef837e43138004c6c10937b506c7e93d4c4e32
Bryan Housel <bryan@mapbox.com>
2020-10-23 11:44:24 -0400

    Add `removeOldNsiClaims` step at the end of the script execution - Find all items in Wikidata with NSI identifier claims (P8253). - Remove any old claims where we don't reference that QID anymore. - This ensures that Wikidata is kept up to date and doesn't contain orphaned   NSI identifiers

--- scripts/build_wikidata.js
+++ scripts/build_wikidata.js
@@ -17,26 +17,9 @@ const wbk = require('wikibase-sdk')({
   sparqlEndpoint: 'https://query.wikidata.org/sparql'
 });
 
-findNSIOrphans()
-  .then(() => {
-    console.log('done');
-    process.exit(1);
-  });
-
-// spin forever
-let spins = 0;
-function spin() {
-  console.log(spins++);
-  return delay(1000)
-    .then(() => spin());
-};
-
-spin();
-return;
-
 
 // set to true if you just want to test what the script will do without updating Wikidata
-const DRYRUN = true;
+const DRYRUN = false;
 
 // First, try to load the user's secrets.
 // This is optional but needed if you want this script to:
@@ -173,8 +156,11 @@ let _urls = wbk.getManyEntities({
 });
 
 let _errors = [];
-doFetch().then(finish);
 
+
+doFetch()
+  .then(removeOldNsiClaims)
+  .then(finish);
 
 
 //
@@ -406,10 +392,6 @@ function processEntities(result) {
         const msg = `Adding NSI identifier for ${qid}: ${nsiIds[i]}`;
         wbEditQueue.push({ qid: qid, id: qid, property: 'P8253', value: nsiIds[i], rank: 'normal', msg: msg });
       }
-
-      // TOOD - This will not catch situations where we have changed the QID on our end,
-      //   because they won't exist in the index anymore and been gathered in the first place.
-      // We should maybe make a SPARQL query to clean up entities with orphaned P8253 claims.
     }
 
   });  // foreach qid
@@ -629,20 +611,34 @@ function fetchFacebookLogo(qid, username) {
 }
 
 
-// Get all items in Wikidata with NSI identifiers
-function findNSIOrphans() {
-  const query = 'SELECT ?qid ?nsi WHERE { ?qid wdt:P8253 ?nsi. }';
+// `removeOldNsiClaims`
+// Find all items in Wikidata with NSI identifier claims (P8253).
+// Remove any old claims where we don't reference that QID anymore.
+function removeOldNsiClaims() {
+  const query = `
+    SELECT ?qid ?nsiId ?guid
+    WHERE {
+      ?qid    p:P8253  ?guid.
+      ?guid  ps:P8253  ?nsiId.
+    }`;
 
   return fetch(wbk.sparqlQuery(query))
     .then(response => {
       if (!response.ok) throw new Error(response.status + ' ' + response.statusText);
       return response.json();
     })
-    .then(result => {
-      const results = wbk.simplify.sparqlResults(result);
-console.log(JSON.stringify(results[0]));
-return true;
+    .then(wbk.simplify.sparqlResults)
+    .then(results => {
+      let wbEditQueue = [];
+      results.forEach(item => {
+        if (!_qidIdItems[item.qid]) {
+          const msg = `Removing old NSI identifier for ${item.qid}: ${item.nsiId}`;
+          wbEditQueue.push({ qid: item.qid, guid: item.guid, msg: msg });
+        }
+      });
+      return wbEditQueue;
     })
+    .then(processWbEditQueue)
     .catch(e => {
       _errors.push(e);
       console.error(colors.red(e));
@@ -650,7 +646,12 @@ return true;
 }
 
 
-// We need to slow these down and run them sequentially with some delay.
+// `processWbEditQueue`
+// Perform any edits to Wikidata that we want to do.
+// (Slow these down and run them sequentially with some delay).
+//
+// Set `DRYRUN=true` at the beginning of this script to prevent actual edits from happening.
+//
 function processWbEditQueue(queue) {
   if (!queue.length) return Promise.resolve();
 
@@ -689,6 +690,9 @@ function processWbEditQueue(queue) {
 }
 
 
+// `enLabelForQID`
+// Pick a value that should be suitable to use as an English label.
+// If we are pushing edits to Wikidata, add en labels for items that don't have them.
 function enLabelForQID(qid) {
   const ids = Array.from(_qidItems[qid]);
   for (let i = 0; i < ids.length; i++) {
