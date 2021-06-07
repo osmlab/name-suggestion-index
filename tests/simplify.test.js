43e50b725db06516b330dcaa888c5f3e70a47faf
Bryan Housel <bhousel@gmail.com>
2021-06-07 19:36:17 -0400

    Drop entire general and supplemental unicode punctuation blocks U+2000-U206F is the General Punctuation block (en/em dashes are here) U+2E00-U2E7F is the Supplemental Punctuation block (closes #5117)

--- tests/simplify.test.js
+++ tests/simplify.test.js
@@ -14,6 +14,13 @@ describe('simplify', () => {
     expect(simplify('Jimmy Choo')).toBe('jimmychoo');
   });
 
+  test('removes various dashes', () => {
+    expect(simplify('PTV - Metropolitan')).toBe('ptvmetropolitan');  // hypen
+    expect(simplify('PTV – Metropolitan')).toBe('ptvmetropolitan');  // en dash (U+2013)
+    expect(simplify('PTV — Metropolitan')).toBe('ptvmetropolitan');  // em dash (U+2014)
+    expect(simplify('PTV ― Metropolitan')).toBe('ptvmetropolitan');  // horizontal bar (U+2015)
+  });
+
   test('removes unprintable unicode (like RTL/LTR marks, zero width space, zero width nonjoiner)', () => {
     expect(simplify('\u200FJim\u200Bmy\u200CChoo\u200E')).toBe('jimmychoo');
   });
