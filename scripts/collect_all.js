6e204083facdd07ca3c89cd88e810cf4692810b4
Bryan Housel <bhousel@gmail.com>
2021-09-02 17:57:52 -0400

    Add some things to create an old but functioning osmium environment (re: #5311)

--- scripts/collect_all.js
+++ scripts/collect_all.js
@@ -10,14 +10,15 @@
 // Please see README.md for more info
 
 // External
-import colors from 'colors/safe.js';
-import fs from 'node:fs';
-import osmium from 'osmium';
-import shell from 'shelljs';
-import stringify from '@aitodotai/json-stringify-pretty-compact';
+const colors = require('colors/safe.js');
+const fs = require('fs');
+const osmium = require('osmium');
+const shell = require('shelljs');
+const stringify = require('@aitodotai/json-stringify-pretty-compact');
 
-// Internal
-import { sortObject } from '../lib/sort_object.js';
+const localeCompare = require('locale-compare');
+const withLocale = localeCompare('en-US');
+
 
 if (process.argv.length < 3) {
   console.log('');
@@ -90,4 +91,27 @@ function collect(tag, fromKeys, threshold) {
 
   fs.writeFileSync(file, stringify(sortObject(all)) + '\n');
   console.timeEnd(END);
+}
+
+// Returns an object with sorted keys and sorted values.
+// (This is useful for file diffing)
+function sortObject(obj) {
+  if (!obj) return null;
+
+  let sorted = {};
+  Object.keys(obj).sort(keyCompare).forEach(k => {
+    sorted[k] = Array.isArray(obj[k]) ? obj[k].sort(withLocale) : obj[k];
+  });
+  return sorted;
+
+  function keyCompare(a, b) {
+    const qid = /^Q(\d+)$/;
+    const aMatch = a.match(qid);
+    const bMatch = b.match(qid);
+    if (aMatch && bMatch) {
+      return parseInt(b[1], 10) - parseInt(a[1], 10);   // sort QIDs numerically
+    } else {
+      return withLocale(a, b);
+    }
+  }
 }
