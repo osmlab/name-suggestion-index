983308090f7d0d42efdfd0916d380b9264abef9e
Bryan Housel <bhousel@gmail.com>
2021-11-02 16:36:34 -0400

    Raise a warning if a same-named item exists in several trees (re: #5625)

--- scripts/build_index.js
+++ scripts/build_index.js
@@ -259,6 +259,20 @@ function loadIndex() {
   matcher.buildMatchIndex(_cache.path);
   console.timeEnd(MATCH_INDEX_END);
 
+  let warnMatched = matcher.getWarnings();
+  if (warnMatched.length) {
+    console.warn(colors.yellow('\n⚠️   Warning - matchIndex errors:'));
+    console.warn(colors.gray('-').repeat(70));
+    console.warn(colors.gray('  `key/value/name` occurs multiple times in the match index.'));
+    console.warn(colors.gray('  To resolve these, make sure the key/value/name does not appear in multiple trees'));
+    console.warn(colors.gray('    (e.g. `amenity/post_office/ups` should not be both a "brand" and an "operator"'));
+    console.warn(colors.gray('-').repeat(70));
+    warnMatched.forEach(w => console.warn(colors.yellow(w)));
+    console.warn('total ' + warnMatched.length);
+  }
+
+
+
   // It takes a few seconds to resolve all of the locationSets into GeoJSON and insert into which-polygon
   // We don't need a location index for this script, but it's useful to know.
   const LOCATION_INDEX_END = '👍  ' + colors.green(`built location index`);
@@ -516,7 +530,6 @@ function checkItems(t) {
   const tree = _config.trees[t];
   const oddChars = /[\s=!"#%'*{},.\/:?\(\)\[\]@\\$\^*+<>«»~`’\u00a1\u00a7\u00b6\u00b7\u00bf\u037e\u0387\u055a-\u055f\u0589\u05c0\u05c3\u05c6\u05f3\u05f4\u0609\u060a\u060c\u060d\u061b\u061e\u061f\u066a-\u066d\u06d4\u0700-\u070d\u07f7-\u07f9\u0830-\u083e\u085e\u0964\u0965\u0970\u0af0\u0df4\u0e4f\u0e5a\u0e5b\u0f04-\u0f12\u0f14\u0f85\u0fd0-\u0fd4\u0fd9\u0fda\u104a-\u104f\u10fb\u1360-\u1368\u166d\u166e\u16eb-\u16ed\u1735\u1736\u17d4-\u17d6\u17d8-\u17da\u1800-\u1805\u1807-\u180a\u1944\u1945\u1a1e\u1a1f\u1aa0-\u1aa6\u1aa8-\u1aad\u1b5a-\u1b60\u1bfc-\u1bff\u1c3b-\u1c3f\u1c7e\u1c7f\u1cc0-\u1cc7\u1cd3\u200b-\u200f\u2016\u2017\u2020-\u2027\u2030-\u2038\u203b-\u203e\u2041-\u2043\u2047-\u2051\u2053\u2055-\u205e\u2cf9-\u2cfc\u2cfe\u2cff\u2d70\u2e00\u2e01\u2e06-\u2e08\u2e0b\u2e0e-\u2e16\u2e18\u2e19\u2e1b\u2e1e\u2e1f\u2e2a-\u2e2e\u2e30-\u2e39\u3001-\u3003\u303d\u30fb\ua4fe\ua4ff\ua60d-\ua60f\ua673\ua67e\ua6f2-\ua6f7\ua874-\ua877\ua8ce\ua8cf\ua8f8-\ua8fa\ua92e\ua92f\ua95f\ua9c1-\ua9cd\ua9de\ua9df\uaa5c-\uaa5f\uaade\uaadf\uaaf0\uaaf1\uabeb\ufe10-\ufe16\ufe19\ufe30\ufe45\ufe46\ufe49-\ufe4c\ufe50-\ufe52\ufe54-\ufe57\ufe5f-\ufe61\ufe68\ufe6a\ufe6b\ufeff\uff01-\uff03\uff05-\uff07\uff0a\uff0c\uff0e\uff0f\uff1a\uff1b\uff1f\uff20\uff3c\uff61\uff64\uff65]+/g;
 
-  let warnMatched = matcher.getWarnings();
   let warnDuplicate = [];
   let warnFormatWikidata = [];
   let warnFormatWikipedia = [];
@@ -656,22 +669,6 @@ function checkItems(t) {
 
     });
   });
-
-  if (warnMatched.length) {
-    console.warn(colors.yellow('\n⚠️   Warning - Ambiguous matches:'));
-    console.warn(colors.gray('-').repeat(70));
-    console.warn(colors.gray('  If the items are the different, make sure they have different locationSets (e.g. "us", "ca"'));
-    console.warn(colors.gray('  If the items are the same, remove extra `matchTags` or `matchNames`.  Remember:'));
-    console.warn(colors.gray('  - Name matching ignores letter case, punctuation, spacing, and diacritical marks (é vs e). '));
-    console.warn(colors.gray('    No need to add `matchNames` for variations in these.'));
-    console.warn(colors.gray('  - Tag matching automatically includes other similar tags in the same match group.'));
-    console.warn(colors.gray('    No need to add `matchTags` for similar tags.  see `config/matchGroups.json`'));
-    console.warn(colors.gray('-').repeat(70));
-    warnMatched.forEach(w => console.warn(
-      colors.yellow('  "' + w[0] + '"') + ' -> matches? -> ' + colors.yellow('"' + w[1] + '"')
-    ));
-    console.warn('total ' + warnMatched.length);
-  }
 
   if (warnMissingTag.length) {
     console.warn(colors.yellow('\n⚠️   Warning - Missing tag:'));
