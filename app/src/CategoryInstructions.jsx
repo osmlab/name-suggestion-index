ab8dd7aae0d18f34839f73f910506ed3134b4539
Chris <59393694+UKChris-osm@users.noreply.github.com>
2021-11-21 23:00:22 +0000

    Flag text.

--- app/src/CategoryInstructions.jsx
+++ app/src/CategoryInstructions.jsx
@@ -5,22 +5,26 @@ export default function CategoryInstructions(props) {
   // setup defaults for this tree..
   const t = props.t;
 
-  let a, itemType, wikidataTag;
+  let a, itemType, logo, wikidataTag;
   if (t === 'brands') {
     a = 'a';
     itemType = 'brand';
+    logo = 'logos';
     wikidataTag = 'brand:wikidata';
   } else if (t === 'flags') {
     a = 'a';
     itemType = 'flag';
+    logo = 'a commons logo';
     wikidataTag = 'flag:wikidata';
   } else if (t === 'operators') {
     a = 'an';
     itemType = 'operator';
+    logo = 'logos';
     wikidataTag = 'operator:wikidata';
   } else if (t === 'transit') {
     a = 'a';
     itemType = 'network';
+    logo = 'logos';
     wikidataTag = 'network:wikidata';
   }
 
@@ -38,7 +42,7 @@ export default function CategoryInstructions(props) {
     <li>Click the "Search Overpass Turbo" link to see where the {itemType} is mapped in OpenStreetMap.</li>
     <li>If a record is missing a <code>'{wikidataTag}'</code> tag, you can do the research to add it to our project, or filter it out if it is not {a} {itemType}.<br/>
       See <a target='_blank' href='https://github.com/osmlab/name-suggestion-index/blob/main/CONTRIBUTING.md'>CONTRIBUTING.md</a> for more info.</li>
-    <li>If a record with a <code>'{wikidataTag}'</code> tag has a poor description or is missing logos, click the Wikidata link and edit the Wikidata page.<br/>{social}</li>
+    <li>If a record with a <code>'{wikidataTag}'</code> tag has a poor description or is missing {logo}, click the Wikidata link and edit the Wikidata page.<br/>{social}</li>
     </ul>
     </div>
     </>
