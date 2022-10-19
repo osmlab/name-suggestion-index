c46333dd5f549334f815a44d8d98642b956fe622
Peter Newman <peterjnewman@gmail.com>
2022-10-19 18:07:04 +0100

    Pull the countryReplacement data back into the build script

--- scripts/build_index.js
+++ scripts/build_index.js
@@ -338,6 +338,11 @@ function saveIndex() {
 // - update all items to have whatever tags they should have.
 //
 function mergeItems() {
+  // Any country codes which should be replaced by more standard ones in the locationSets
+  const countryReplacements = {
+    'uk': 'gb',  // Exceptionally reserved, United Kingdom is officially assigned the alpha-2 code GB
+  }
+
   const START = '🏗   ' + chalk.yellow(`Merging items...`);
   const END = '👍  ' + chalk.green(`done merging`);
   console.log('');
@@ -538,9 +543,9 @@ function mergeItems() {
             }
           }
         });
-        
+
         // Perform locationSet country code replacements
-        Object.keys(_config.countryReplacements).forEach(country => {
+        Object.keys(countryReplacements).forEach(country => {
           [item.locationSet.include, item.locationSet.exclude].forEach(v => {
             if (v) {
               normalizeCountryCode(v, country);
@@ -577,7 +582,7 @@ function mergeItems() {
   function normalizeCountryCode(countries, country) {
     const index = countries.indexOf(country.toLowerCase())
     if (index >= 0) {
-      const replace = _config.countryReplacements[country.toLowerCase()];
+      const replace = countryReplacements[country.toLowerCase()];
       if (replace && replace.country !== undefined) {
         countries[index] = replace.country.toLowerCase()
       }
