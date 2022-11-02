81de4dffddf4b81187a9978a47a1ad245a5f3a92
mcliquid <info@mcliquid.de>
2022-11-02 20:02:47 +0100

    Revert "Merge remote-tracking branch 'upstream/main' into vvv"
    This reverts commit a2354e75eab2f88cf46a051e58b8239d287c99e0, reversing
    changes made to 43522b493e91cbbc19e2167bf9b9ae99cc066c88.

--- scripts/build_features.js
+++ scripts/build_features.js
@@ -54,8 +54,6 @@ function collectFeatures() {
   let files = {};
 
   glob.sync('features/**/*', { nodir: true }).forEach(file => {
-    if (/\.md$/i.test(file)) return;  // ignore markdown/readme files - #7292
-
     if (!/\.geojson$/.test(file)) {
       console.error(chalk.red(`Error - file should have a .geojson extension:`));
       console.error('  ' + chalk.yellow(file));
