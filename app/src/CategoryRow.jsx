a669b646dcf3c63aec381d9a7a6a815b0dd5abd2
UKChris-osm <59393694+UKChris-osm@users.noreply.github.com>
2023-02-21 12:31:56 +0000

    Add item.fromTemplate checks

--- app/src/CategoryRow.jsx
+++ app/src/CategoryRow.jsx
@@ -283,8 +283,48 @@ relation[${k}=${v}][network:wikidata=${qid}]
 `;
     });
 
-    if (item.matchNames || item.matchTags || item.note || item.preserveTags)
+    /* Add an <hr/> line break only if addational information will be displayed. */
+    if (item.matchNames || item.matchTags || item.note || item.preserveTags || item.fromTemplate)
       result += '<hr/>';
+    
+    /* Are the items being drawn from a template? 'item.fromTemplate' is set to true in nsi.json if templated. */
+    if (item.fromTemplate) {
+      let url,text;
+
+      /* Brands */
+      if ((t=='brands') && (k=='amenity') && (v=='atm'))
+        {url = '/index.html?t=brands&amp;k=amenity&amp;v=bank'; text = '/brands/amenity/bank.json';}
+
+      /* Operators */
+      if ((t=='operators') && (k=='leisure') && (v=='nature_reserve'))
+        {url = '/index.html?t=operators&amp;k=leisure&amp;v=park'; text = '/operators/leisure/park.json';}
+
+      if ((t=='operators') && (k=='power') && ((v=='minor_line') || (v=='pole') || (v=='tower')))
+        {url = '/index.html?t=operators&amp;k=power&amp;v=line'; text = '/operators/power/line.json';}
+      if ((t=='operators') && (k=='power') && (v=='transformer'))
+        {url = '/index.html?t=operators&amp;k=power&amp;v=substation'; text = '/operators/power/substation.json';}
+
+      if ((t=='operators') && (k=='man_made') && ((v=='water_tower') || (v=='water_works')))
+        {url = '/index.html?t=operators&amp;k=office&amp;v=water_utility'; text = '/operators/office/water_utility.json';}
+
+      /* Transit */
+      if ((t=='transit') && (k=='highway') && (v=='bus_stop'))
+        {url = '/index.html?t=transit&amp;k=route&amp;v=bus'; text = '/transit/route/bus.json';}
+
+      if ((t=='transit') && (k=='amenity') && (v=='ferry_terminal'))
+        {url = '/index.html?t=transit&amp;k=route&amp;v=ferry'; text = '/transit/route/ferry.json';}
+
+      if ((t=='operators') && (k=='amenity') && (v=='post_box')) {
+        /* Post Boxes use multiple templates */
+        result += '<strong>Master templates:</strong><br/>';
+        result += '<a href="/index.html?t=brands&amp;k=amenity&amp;v=post_office">/brands/amenity/post_office.json</a><br/>';
+        result += '<a href="/index.html?t=operators&amp;k=amenity&amp;v=post_office">/operators/amenity/post_office.json</a><br/>';
+      } else {
+        /* All the rest use a single template */
+        result += '<strong>Master template:</strong><br/>';
+        result += '<a href="' + url + '">' + text + '</a><br/>';
+      }
+    }
 
     if (item.matchNames)
       result += '<strong>matchNames</strong>:<br/>' + item.matchNames + '<br/>';
