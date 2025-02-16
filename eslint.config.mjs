8c59fab6d92862e29e2d4f9482491df1c4a37416
Logan Rosen <loganrosen@gmail.com>
2025-02-15 19:21:11 -0500

    Simplify config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,7 @@
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [...compat.extends("eslint:recommended"), {
+export default [js.configs.recommended, {
     languageOptions: {
         globals: {
             ...globals.node,
