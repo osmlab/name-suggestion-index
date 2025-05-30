57e0faf0749bf42b6fc1b369d54a2bd57bc91480
Snowysauce <22599992+Snowysauce@users.noreply.github.com>
2025-05-29 22:55:47 -0400

    adjusted category_mast template to exclude certain railroads
    * excluded North American railroads without overhead electric lines from the power/category_mast template (#10839)
    * fixed the syntax for the generic exclusion array in operators/route/railway and operators/route/tracks
    * fixed broken icon link in scripts\dist_files (uncontroversial portion of #10601)

--- scripts/dist_files.js
+++ scripts/dist_files.js
@@ -511,7 +511,7 @@ function buildTaginfo() {
       'description': 'Canonical features for OpenStreetMap',
       'project_url': 'https://github.com/osmlab/name-suggestion-index',
       'doc_url': 'https://github.com/osmlab/name-suggestion-index/blob/main/README.md',
-      'icon_url': 'https://cdn.jsdelivr.net/npm/@mapbox/maki@6/icons/fastr-food-15.svg',
+      'icon_url': 'https://cdn.jsdelivr.net/npm/@mapbox/maki@6/icons/fast-food-15.svg',
       'contact_name': 'Bryan Housel',
       'contact_email': 'bhousel@gmail.com'
     }
